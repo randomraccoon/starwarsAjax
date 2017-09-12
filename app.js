2cf95b39bbd90fbf6c6c4fb1d1475a3e3a6bbcf8
Cameron Earl <developer.cameron@gmail.com>
2017-09-12 14:29:15 -0700

    Complete exercise

--- app.js
+++ app.js
@@ -1,11 +1,62 @@
-$(document).ready(function(){
-  $.get('https://swapi.co/api/films/7/', function(data){
-    console.log(data);
-    let firstHeader = $("<h3></h3>")[0];
-    $(firstHeader).text(data.title);
-    $('.movie')[0].append(firstHeader);
-    let opening = $("<p></p>")[0];
-    $(opening).text(data.opening_crawl);
-    $('.movie')[0].append(opening);
-  })
-})
+let infoTitle;
+
+function showPeople(movie) {
+  $('#info-box').text("");
+  $(infoTitle).text('People of ' + movie.title);
+  let firstThree = movie.characters.slice(0,3);
+  for (let url of firstThree) {
+    $.get(url,(person)=> {
+      let p = $('<p></p>');
+      $(p).text(person.name);
+      $('#info-box').append(p);
+    });
+  }
+}
+function showPlanets(movie) {
+  $('#info-box').text("");
+  $(infoTitle).text('Planets of ' + movie.title);
+  let firstThree = movie.planets.slice(0,3);
+  for (let url of firstThree) {
+    $.get(url,(planet)=> {
+      let p = $('<p></p>');
+      $(p).text(planet.name);
+      $('#info-box').append(p);
+    });
+  }
+}
+
+$(document).ready(()=> {
+  infoTitle = $('#info-title')[0];
+  $(infoTitle).text("");
+  for (let i = 0; i < 6; i++) {
+    $.get('https://swapi.co/api/films/' + (i+1), (data)=>{
+      // console.log(data);
+      let container = $('.movie')[i];
+      let header = $('<h3></h3>')[0];
+      $(header).text(data.title);
+      container.append(header);
+      $(header).addClass('title');
+
+      let p = $('<p></p>')[0];
+      $(p).text(data.opening_crawl);
+      container.append(p);
+
+      let btnContainer = $('<div></div>')[0];
+      $(btnContainer).append(createButton('People',data));
+      $(btnContainer).append(createButton('Planets',data));
+      container.append(btnContainer);
+    });
+  }
+});
+
+function createButton(query, data) {
+  let btn = $('<div></div>')[0];
+  $(btn).addClass('button');
+  $(btn).text('show ' + query);
+  let funcName = 'show'+query;
+  let callback = window[funcName];
+  $(btn).click(()=>{
+    callback(data);
+  });
+  return btn;
+}
